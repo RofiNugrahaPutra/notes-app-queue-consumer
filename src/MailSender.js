3f23ed0796d9315585e436979dcf7a5615db10cf
Rofi Nugraha Putra <rofinugrahaputra@gmail.com>
2023-10-04 21:25:02 +0700

    consumer program finished

---
+++ src/MailSender.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+
+/* eslint no-underscore-dangle: 0 */
+class MailSender {
+  constructor() {
+    this._transporter = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      post: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    });
+  }
+
+  sendEmail(targetEmail, content) {
+    const message = {
+      from: 'Notes Apps',
+      to: targetEmail,
+      subject: 'Export Catatan',
+      text: 'Terlampir hasil dari export catatan',
+      attachments: [
+        {
+          filename: 'notes.json',
+          content,
+        },
+      ],
+    };
+
+    return this._transporter.sendMail(message);
+  }
+}
+
+module.exports = MailSender;
